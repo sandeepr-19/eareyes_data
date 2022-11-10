4383845d275129fea798998189373bc5e349c923
sandeep-19 <sandeep.rqs@gmail.com>
2022-11-10 19:48:33 +0530

    repo initilize

---
+++ index.js
@@ -0,0 +1,332 @@
+const express = require("express");
+const PORT = process.env.PORT || 8031;
+const app = express();
+const pug = require("pug");
+const path = require("path");
+const bdp = require("body-parser");
+const enc = bdp.urlencoded({ extended: true });
+const xl = require("excel4node");
+const wb = new xl.Workbook();
+const ws = wb.addWorksheet("Worksheet Name");
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "./views"));
+app.use(express.static("./public"));
+
+const MongoClient = require("mongodb").MongoClient;
+const db_url =
+  "mongodb+srv://sandeep:aZMYo0C6G3KxYqMA@cluster-details.ffmkvzn.mongodb.net/?retryWrites=true&w=majority";
+
+app.get("/", (req, res) => {
+  res.render("home.pug");
+});
+
+app.get("/event1", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event1").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        team_name: element["team_name"],
+        name1: element["name1"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+        title: element["title"],
+        abstract: element["abstract"],
+        name2: element["name2"],
+        rollno2: element["rollno2"],
+        name3: element["name3"],
+        rollno3: element["rollno3"],
+      });
+    });
+
+    const headingColumnNames = [
+      "team_name",
+      "name1",
+      "rollno",
+      "email",
+      "mobileno",
+      "title",
+      "abstract",
+      "name2",
+      "rollno2",
+      "name3",
+      "rollno3",
+    ];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event1.xlsx", () => {
+      const file = `${__dirname}/event1.xlsx`;
+      res.download(file);
+    });
+    db.close();
+  });
+});
+
+app.get("/event2", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event2").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        team_name: element["team_name"],
+        name1: element["name1"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+        title: element["title"],
+        abstract: element["abstract"],
+        name2: element["name2"],
+        rollno2: element["rollno2"],
+        name3: element["name3"],
+        rollno3: element["rollno3"],
+      });
+    });
+
+    const headingColumnNames = [
+      "team_name",
+      "name1",
+      "rollno",
+      "email",
+      "mobileno",
+      "title",
+      "abstract",
+      "name2",
+      "rollno2",
+      "name3",
+      "rollno3",
+    ];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event2.xlsx", () => {
+      const file = `${__dirname}/event2.xlsx`;
+      res.download(file);
+    });
+    db.close();
+  });
+});
+
+app.get("/event3", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event3").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        name1: element["name"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+      });
+    });
+
+    const headingColumnNames = ["name", "rollno", "email", "mobileno"];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event3.xlsx", () => {
+      const file = `${__dirname}/event3.xlsx`;
+      res.download(file);
+    });
+
+    db.close();
+  });
+});
+
+app.get("/event4", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event4").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        name1: element["name"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+      });
+    });
+
+    const headingColumnNames = ["name", "rollno", "email", "mobileno"];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event4.xlsx", () => {
+      const file = `${__dirname}/event4.xlsx`;
+      res.download(file);
+    });
+
+    db.close();
+  });
+});
+
+app.get("/event5", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event5").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        team_name: element["team_name"],
+        name1: element["name1"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+        name2: element["name2"],
+        rollno2: element["rollno2"],
+        name3: element["name3"],
+        rollno3: element["rollno3"],
+      });
+    });
+
+    const headingColumnNames = [
+      "team_name",
+      "name1",
+      "rollno",
+      "email",
+      "mobileno",
+      "name2",
+      "rollno2",
+      "name3",
+      "rollno3",
+    ];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event5.xlsx", () => {
+      const file = `${__dirname}/event5.xlsx`;
+      res.download(file);
+    });
+
+    db.close();
+  });
+});
+
+app.get("/event6", (req, res) => {
+  MongoClient.connect(db_url, async (err, db) => {
+    if (err) throw err;
+    const dbo = db.db("event-details");
+    let cursor1 = await dbo.collection("event6").find({});
+    let values1 = await cursor1.toArray();
+    let details = [];
+    values1.forEach((element) => {
+      details.push({
+        team_name: element["team_name"],
+        name1: element["name1"],
+        rollno: element["rollno"],
+        email: element["email"],
+        mobileno: element["mobileno"],
+        name2: element["name2"],
+        rollno2: element["rollno2"],
+        name3: element["name3"],
+        rollno3: element["rollno3"],
+      });
+    });
+
+    const headingColumnNames = [
+      "team_name",
+      "name1",
+      "rollno",
+      "email",
+      "mobileno",
+      "name2",
+      "rollno2",
+      "name3",
+      "rollno3",
+    ];
+    //Write Column Title in Excel file
+    let headingColumnIndex = 1;
+    headingColumnNames.forEach((heading) => {
+      ws.cell(1, headingColumnIndex++).string(heading);
+    });
+    //Write Data in Excel file
+    let rowIndex = 2;
+    details.forEach((record) => {
+      let columnIndex = 1;
+      Object.keys(record).forEach((columnName) => {
+        ws.cell(rowIndex, columnIndex++).string(record[columnName]);
+      });
+      rowIndex++;
+    });
+    wb.write("event6.xlsx", () => {
+      const file = `${__dirname}/event6.xlsx`;
+      res.download(file);
+    });
+
+    db.close();
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
---
+++ public/home.js
@@ -0,0 +1,23 @@
+document.getElementById("down1").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event1";
+});
+
+document.getElementById("down2").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event2";
+});
+
+document.getElementById("down3").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event3";
+});
+
+document.getElementById("down4").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event4";
+});
+
+document.getElementById("down5").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event5";
+});
+
+document.getElementById("down6").addEventListener("click", () => {
+  window.location.href = window.location.origin + "/event6";
+});
